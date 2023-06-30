339b38b6fc998ba528346cfed3cb911cfdad8462
sam <sam@prompt.limited>
2023-06-30 12:48:04 +0100

    chore: Update action to run on `node16` (#13)
    Co-Authored-By: Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,9 @@ async function run(): Promise<void> {
 
     core.setOutput('csv', aliases.join(','))
     core.setOutput('list', aliases.join('\n'))
-  } catch (error) {
-    core.setFailed(error.message)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    core.setFailed(error.message || 'An unknown error occurred.')
   }
 }
 
